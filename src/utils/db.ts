047947079ceaa4ef79a7da312f0506869123de13
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2024-11-15 22:11:28 +0100

    Fix dates stuff

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,5 +1,5 @@
 import {User} from "../types/db.js";
 
 export async function upsertUser(did: string, day: number, month: number) {
-    await User.upsert({did, day, month})
+    await User.upsert({did, day, month, nextUpdate: null})
 }
