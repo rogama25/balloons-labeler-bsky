27f7cc9d8cb67ba9e162baa43a180521fcc9cd84
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2024-11-15 18:27:44 +0100

    Fix upsert

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,5 @@
 import {User} from "../types/db.js";
 
 export async function upsertUser(handle: string, day: number, month: number) {
-    const user = new User({handle, day, month})
-    await user.save()
+    await User.upsert({handle, day, month})
 }
